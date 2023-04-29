75f33e914442681941fd0d70a7bb524b597f4c2d
s204431 <s204431@student.dtu.dk>
2023-04-29 11:32:35 +0200

    Final

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,15 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         BeliefBase beliefBase = new BeliefBase();
-        String formula = "(p -> q) <-> (!q -> !p)";
-        //String formula = "((p & q) -> r) & (p -> !r) & (q -> !r) -> !r";
-        Formula parsed = Formula.parseString(formula);
-        System.out.println("Parsing formula: " + formula);
-        System.out.println();
-        System.out.println("Internal representation: " + parsed);
-        System.out.println("Pretty print: " + parsed.prettyPrint());
-        System.out.println("Is valid: " + beliefBase.resolution(parsed));
-        
         new UI(beliefBase);
     }
 }
